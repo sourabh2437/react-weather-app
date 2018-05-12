5b3a55e3993978e62d76426620206dfe7cd8c88d
Sourabh Kulkarni <sourabh.kulkarni@sap.com>
2018-05-12 23:28:12 +0530

    First commit

---
+++ src/components/Weather.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class Weather extends Component{
+    render(){
+        return (
+            <div className="weather__info">
+            { this.props.city && this.props.country && <p className="weather__key"> Location :  
+                <span className="weather__value"> {this.props.city}, {this.props.country}</span> 
+                </p>
+            }
+            
+            {this.props.temperature && <p className="weather__key"> Temperature :  
+                <span className="weather__value"> {this.props.temperature}</span>
+                </p>
+            }
+            
+            {this.props.humidity && <p className="weather__key"> Humidity :  
+                <span className="weather__value"> {this.props.humidity}</span>
+                </p>
+            }
+            
+            {this.props.condition && <p className="weather__key"> Conditions :  
+                <span className="weather__value"> {this.props.condition}</span>
+                </p> 
+            }
+            
+            {this.props.error && <p className="weather__error">  {this.props.error}</p>}
+            
+            </div>
+            
+        )
+    }
+}
+export default Weather;
